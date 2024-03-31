4fc5e6be77aa6af218be7b89c78ee02e2a83f2ee
wiseman88 <wiseadam02@gmail.com>
2024-03-31 08:51:11 +0200

    feat(components): add lights component
    - Added Lights component to create a light source positioned at a specific point
    in the scene that emits light in a specific direction.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 import Navbar from './components/Navbar'
 
 const App = () => {
-
   return (
     <main className='bg-black'>
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
