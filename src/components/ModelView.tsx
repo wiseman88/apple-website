becc0c55682c22e627ec65c1a8892b629c18d188
wiseman88 <wiseadam02@gmail.com>
2024-03-31 08:58:09 +0200

    feat(components): add loader component

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -1,6 +1,7 @@
 import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
 import Lights from './Lights'
 import IPhone from './IPhone'
+import Loader from './Loader'
 import { Suspense } from 'react'
 import * as THREE from 'three'
 
@@ -52,13 +53,7 @@ const ModelView: React.FC<ModelViewProps> = ({
         name={`${index === 1} ? 'small': 'large'`}
         position={[0, 0, 0]}
       >
-        <Suspense
-          fallback={
-            <Html>
-              <div>Loading</div>
-            </Html>
-          }
-        >
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
