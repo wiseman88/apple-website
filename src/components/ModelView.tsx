40f20109c86a98a2f6f4bd8eb6de098d6dcceaa5
wiseman88 <wiseadam02@gmail.com>
2024-03-31 08:49:33 +0200

    feat(components): add modelview component
    - Added ModelView components for displaying current selected
    iphone color variant.

---
+++ src/components/ModelView.tsx
@@ -0,0 +1,73 @@
+import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
+import Lights from './Lights'
+import IPhone from './IPhone'
+import { Suspense } from 'react'
+import * as THREE from 'three'
+
+interface ModelViewProps {
+  index: number
+  groupRef: React.RefObject<any>
+  gsapType: string
+  controlRef: React.RefObject<any>
+  setRotationState: (size: number) => void
+  size: number
+  item: any
+}
+
+const ModelView: React.FC<ModelViewProps> = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%]' : ''`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small': 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading</div>
+            </Html>
+          }
+        >
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  )
+}
+
+export default ModelView
