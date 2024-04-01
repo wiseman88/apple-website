eeb77540d629264ae0972640e1b6ad2d626da4f2
wiseman88 <wiseadam02@gmail.com>
2024-04-01 10:05:29 +0200

    chore(sentry): add sentry
    - Added sentry for error tracking and performance monitoring
    of website.

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+//...
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://65e16daa24c825a2f250a3be9cc2a8e8@o4507010233597952.ingest.us.sentry.io/4507010236874752',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
